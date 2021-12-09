8df9d4670d63a705cc24015f70b08b7a165daef5
bclark629 <65756063+bclark629@users.noreply.github.com>
2021-12-09 11:34:31 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const {prefix} = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+function init(){
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		client.commands.set(command.name, command);
+	}
+	client.on('message', message => {
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		const commandName = args.shift().toLowerCase();
+
+		const command = client.commands.get(commandName)
+				|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+		if (!command) return;
+
+		try {
+			command.execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
+		}
+	});
+
+	client.login(process.env.BOT_TOKEN);
+}
+init();
+
+client.once('ready', () => {
+	console.log('Ready!');
+
+});
